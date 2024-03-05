fbae65eaeb647d73e361a735feb667c1de0596da
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-03-05 14:28:55 +0100

    [add] array di oggetti

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Our Team
 Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 ## Svolgimento
-- 
+- creo l'array di oggetti 
+- ogni oggrtto sarà caratterizzato da 3 proprietà: nome, ruolo e foto
 
---
+++ js/script.js
@@ -0,0 +1,38 @@
+const team=[
+  {
+    firstName:'Wayne Barnett',
+    role:'Founder & CEO',
+    photo:'img/wayne-barnett-founder-ceo.jpg'
+
+  },
+  {
+    firstName:'Angela Caroll',
+    role:'Chief Editor',
+    photo:'img/angela-caroll-chief-editor.jpg'
+
+  },
+  {
+    firstName:'Walter Gordon',
+    role:'Office Manager',
+    photo:'img/walter-gordon-office-manager.jpg'
+
+  },
+  {
+    firstName:'Angela Lopez',
+    role:'Social Media Manager',
+    photo:'img/angela-lopez-social-media-manager.jpg'
+
+  },
+  {
+    firstName:'Scott Estrada',
+    role:'Developer',
+    photo:'img/scott-estrada-developer.jpg'
+
+  },
+  {
+    firstName:'Barbara Ramos',
+    role:'Graphic Designer',
+    photo:'img/barbara-ramos-graphic-designer.jpg'
+
+  }
+]
